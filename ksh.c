c2f7bd6d3b22f5663b883edf54faccffd2edfabd
Reuben Ninan <intelphysic@gmail.com>
2021-01-24 18:05:44 -0700

    commands able to execute, some bugs with background processing

--- ksh.c
+++ ksh.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_TOKEN_SIZE 64
 #define DELIMETERS " \n\t"
+#define BACKGROUND 0;
+#define FOREGROUND 1;
+
+int mode = FOREGROUND;
 
 char *read_input(void)
 {
@@ -13,6 +19,8 @@ char *read_input(void)
     size_t len = 0;
 
     getline(&input, &len, stdin);
+
+    input[strlen(input) - 1] = '\0';
 
     return input;
 }
@@ -26,6 +34,11 @@ void clean_exit(int status)
 char **split_line(char *input)
 {
     char **tokens = malloc(MAX_TOKEN_SIZE * sizeof(char *));
+    // char *input_copy = (char *) malloc(strlen(input) + 1);
+
+    // strcpy(input_copy, input);
+    // input_copy[strlen(input)] = '\0';
+
     char *token;
 
     int idx = 0;
@@ -39,21 +52,73 @@ char **split_line(char *input)
         // add code for more memory allocation...
     }
 
+    // free(input_copy);
+    // input_copy = NULL;
+
     return tokens;
+}
+
+// DEBUGGING FUNCTION
+void print_args(char **args)
+{
+    for (int i = 0; args[i] != NULL; i++)
+        printf("args[%2d]: %s\n", i, args[i]);
+}
+
+void backgroundCheck(char *cmd_line)
+{
+    printf("Last Char: %c\n", cmd_line[strlen(cmd_line) - 1]);
+    if (cmd_line[strlen(cmd_line) - 1] == '&')
+    {
+        mode = BACKGROUND;
+        cmd_line[strlen(cmd_line) - 1] = '\0';
+    }
+    else
+    {
+        mode = FOREGROUND;
+    }
+    printf("Mode: %d\n", mode);
+}
+
+int run_command(char **args)
+{
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        execvp(args[0], args);
+        fputs("\n", stdout);
+    }
+    else if (pid < 0)
+    {
+        fputs("ERROR in fork() system call", stderr);
+        return 0;
+    }
+    else
+    {
+        if (mode)
+        {
+            wait(NULL);
+        }
+        else
+        {
+            printf("[%d]\n", pid);
+        }
+    }
+
+    return 1;
 }
 
 int execute_args(char **args)
 {
 
-    // if (!strcmp(args[0], "exit")){
+    if (!strcmp(args[0], "exit"))
+        return 0;
 
-    //     return 0;
-    // }
+    if (args[0] == NULL)
+        return 1;
 
-    // if (args[0] == NULL)
-    //     return 1;
-
-    return 1;
+    return run_command(args);
 }
 
 int main(void)
@@ -66,7 +131,7 @@ int main(void)
 
     do
     {
-        printf("KShell:~$ ");
+        printf("KShell: $ ");
         fflush(stdout);
 
         /**
@@ -78,13 +143,9 @@ int main(void)
 
         line = read_input();
         args = split_line(line);
+        backgroundCheck(line);
+        print_args(args);
         should_run = execute_args(args);
-        printf("%d\n", should_run);
-        // Debug line split -> args
-        // for (int i = 0; args[i] != NULL; i++)
-        // {
-        //     printf("args[%2d]: %s\n", i, args[i]);
-        // }
 
         free(line);
         free(args);
