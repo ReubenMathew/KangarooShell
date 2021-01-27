6f98b87fd15123b9d95e834f7825bf7463424147
Reuben Ninan <intelphysic@gmail.com>
2021-01-26 19:28:42 -0700

    added history feature

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # KangarooShell
 Simple shell interface
+
+### Deliverables
+- [x] Creating the child process and executing the command in the child
+- [x] Providing a history feature
+- [ ] Implement `cd` functionality with `chdir()` system call
+- [ ] Adding support of input and output redirection
+- [ ] Allowing the parent and child processes to communicate via a pipe
--- ksh.c
+++ ksh.c
@@ -6,12 +6,14 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+#define MAX_TOKENS 80
 #define MAX_TOKEN_SIZE 128
 #define DELIMETERS " "
 #define BACKGROUND 0;
 #define FOREGROUND 1;
 
 int mode = FOREGROUND;
+char **history_args;
 
 char *read_input(void)
 {
@@ -27,6 +29,7 @@ char *read_input(void)
 
 void clean_exit(int status)
 {
+    free(history_args);
     fputs("Closing Shell... Goodbye\n", stdout);
     exit(status);
 }
@@ -58,10 +61,16 @@ char **split_line(char *input)
 }
 
 // DEBUGGING FUNCTION
-void print_args(char **args)
+void print_args_debug(char **args)
 {
     for (int i = 0; args[i] != NULL; i++)
         printf("args[%2d]: %s\n", i, args[i]);
+}
+
+void print_history_args(char **args)
+{
+    for (int i = 0; args[i] != NULL; i++)
+        printf("Running last command: %s \n", args[i]);
 }
 
 void backgroundCheck(char *cmd_line)
@@ -79,7 +88,7 @@ void backgroundCheck(char *cmd_line)
     {
         mode = FOREGROUND;
     }
-    printf("Mode: %d\n", mode);
+    // printf("Mode: %d\n", mode);
 }
 
 int run_command(char **args)
@@ -87,6 +96,29 @@ int run_command(char **args)
     pid_t pid = fork();
     int status;
     int devNull, out2devNull, err2devNull;
+
+    // Add command arguments to history
+    /*                              */
+    if (history_args)
+    {
+        // print_args_debug(history_args);
+        // print_args_debug(args);
+    }
+    else
+    {
+        // Initialize history_args
+        history_args = malloc(MAX_TOKENS * sizeof(char *));
+    }
+
+    // history_args = args
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (history_args[i] == NULL)
+        {
+            history_args[i] = malloc(strlen(args[i]));
+        }
+        memcpy(history_args[i], args[i], strlen(args[i]));
+    }
 
     if (pid == 0)
     {
@@ -101,11 +133,11 @@ int run_command(char **args)
             }
             out2devNull = dup2(devNull, STDOUT_FILENO);
             err2devNull = dup2(devNull, STDERR_FILENO);
-            if (out2devNull == -1 || err2devNull == -1){
-                fputs("Error Reassigning: STDOUT/STDERR\n",stdout);
+            if (out2devNull == -1 || err2devNull == -1)
+            {
+                fputs("Error Reassigning: STDOUT/STDERR\n", stdout);
                 exit(EXIT_FAILURE);
             }
-            
         }
         // Execute command with arguments
         if (execvp(args[0], args) < 1)
@@ -125,7 +157,7 @@ int run_command(char **args)
         }
         else
         {
-            printf("[%d]: %s\n", pid, args[0]);
+            printf("Background PID for [%s]: %d\n", args[0], pid);
         }
     }
 
@@ -134,10 +166,25 @@ int run_command(char **args)
 
 int execute_args(char **args)
 {
+    // Empty newline
     if (args[0] == NULL)
         return 1;
+    // Exit Shell
     if (!strcmp(args[0], "exit"))
         return 0;
+    // Run last command with !!
+    if (!strcmp(args[0], "!!"))
+    {
+        if (!history_args)
+        {
+            fputs("No commands in history.\n", stdout);
+        }
+        else
+        {
+            // print_args(history_args);
+            return run_command(history_args);
+        }
+    }
 
     return run_command(args);
 }
@@ -153,14 +200,8 @@ int main(void)
     do
     {
         printf("KShell: $ ");
+        fflush(stdin);
         fflush(stdout);
-
-        /**
-          * After reading user input, the steps are:
-          * (1) fork a child process
-          * (2) the child process will invoke execvp()
-          * (3) if command includes &, parent and child will run concurrently
-          */
 
         line = read_input();
         backgroundCheck(line);
